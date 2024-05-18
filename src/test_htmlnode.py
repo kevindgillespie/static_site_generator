a2c6c5ab77e12686220796c4a0b24c7b1752b857
Kevin Gillespie <kevinragillespie@gmail.com>
2024-05-18 11:14:42 +0100

    added test for html node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,6 +2,12 @@ import unittest
 
 from htmlnode import HTMLNode
 
-class TestHTMLNode:
+class TestHTMLNode(unittest.TestCase):
 
+    def test_props_to_html(self):
+        html_node = HTMLNode(props = { "someKey": "someValue"})
+        expected_str = 'someKey="someValue"'
+        self.assertEqual(html_node.props_to_html(), expected_str)
 
+if __name__ == "__main__":
+    unittest.main()
