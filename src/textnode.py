571035b9a79ed83d277ce40ef0932d5c3e846ea4
Kevin Gillespie <kevinragillespie@gmail.com>
2024-05-20 16:55:00 +0100

    add functionality for converting TextNode to HTMLNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,13 @@
+from leafnode import LeafNode
+
 class TextNode:
+    text_type_text = "text"
+    text_type_bold = "bold"
+    text_type_italic = "italic"
+    text_type_code = "code"
+    text_type_link = "link"
+    text_type_image = "image"
+
     def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
@@ -12,3 +21,17 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
+    def text_node_to_html_node(text_node):
+        if text_node.text_type == TextNode.text_type_text:
+            return LeafNode(value=text_node.text)
+        elif text_node.text_type == TextNode.text_type_bold:
+            return LeafNode(value=text_node.text, tag="b")
+        elif text_node.text_type == TextNode.text_type_italic:
+            return LeafNode(value=text_node.text, tag="i")
+        elif text_node.text_type == TextNode.text_type_code:
+            return LeafNode(value=text_node.text, tag="code")
+        elif text_node.text_type == TextNode.text_type_link:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        elif text_node.text_type == TextNode.text_type_bold:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        raise Exception("Invalid text type given for HTML")
